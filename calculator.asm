45ee3235131b447bcb99bb535f6f7843f2b4321c
Connor A. Hopley <150775653+Conron4@users.noreply.github.com>
2023-12-12 20:07:34 +1100

    Update calculator.asm with comments

--- calculator.asm
+++ calculator.asm
@@ -1,18 +1,21 @@
 section .data
-    startmessage: dw "Enter the mode(+ - / * sqrt): ", 0
-    retcall: dw "Do you want another calculation(Y/N): ", 0
-    num1: dw "Enter the first number: ", 0
-    num2: dw "Enter the second number: ", 0 
-    num0: dw "Enter the number: ", 0 
-    formatin: db "%lf", 0
-    sqrti: db "%d", 0
-    numout: db "Result: %lf", 10, 0
-    start: dd 0 ; 32-bits integer = 4 bytes
-    cint1: dq 0.0 
-    cint2: dq 0.0 
-    retr: dd 0  
-    wordin: dd "%s", 0
-    clear_command: db "cls", 0
+    ; Messages displayed to the user
+    startmessage: dw "Enter the mode(+ - / * sqrt): ", 0  ; Prompt for entering the calculation mode
+    retcall: dw "Do you want another calculation(Y/N): ", 0  ; Prompt for another calculation
+    num1: dw "Enter the first number: ", 0  ; Prompt for the first number
+    num2: dw "Enter the second number: ", 0  ; Prompt for the second number
+    num0: dw "Enter the number: ", 0  ; Prompt for a number (used in sqrt)
+    formatin: db "%lf", 0  ; Format string for input as a double
+    numout: db "Result: %lf", 10, 0  ; Output format string for displaying the result
+
+    ; Variables for data storage
+    start: dd 0 ; ;Selection Variable
+    cint1: dq 0.0  ; Variable to store the first number for calculation
+    cint2: dq 0.0  ; Variable to store the second number for calculation
+    retr: dd 0  ; Variable to store the response for another calculation
+    wordin: dd "%s", 0  ; Format string for input as a string
+    clear_command: db "cls", 0  ; Command to clear the console
+
 section .text
    global _main 
    extern _scanf 
@@ -20,81 +23,92 @@ section .text
    extern _system
 
 _clear_console:
-    pusha
-    push clear_command
-    call _system
-    add esp, 4
-    popa
-    ret
+    ; Function to clear the console
+    pusha  ; Preserve registers
+    push clear_command  ; Push the command to clear the console
+    call _system  ; Call system to clear the console
+    add esp, 4  ; Adjust the stack pointer after clearing
+    popa  ; Restore the registers
+    ret  ; Return
 
 _main:
-    call _clear_console
-    mov esp, ebp
-    push ebx ; save registers
+    call _clear_console  ; Clear the console
+    mov esp, ebp  ; Set up stack frame
+    push ebx ; Save registers
     push ecx
-    push startmessage 
+
+    ; Display the prompt for entering the calculation mode
+    push startmessage  
     call _printf
 
-    add esp, 4 
-    push start 
-    push wordin 
-    call _scanf
-    mov ebx, dword [start]
-    cmp ebx, '+'
-    je _add
-    cmp ebx, '-'
-    je _sub
-    cmp ebx, '*'
+    add esp, 4  ; Adjust the stack pointer
+
+    push start  ; Push the start mode
+    push wordin  ; Push the formatting
+    call _scanf  ; Read the calculation mode input
+
+    mov ebx, dword [start]  ; Move the input mode to ebx
+    cmp ebx, '+'  ; Compare with signs and jump to appropriate function 
+    je _add  
+    cmp ebx, '-' 
+    je _sub  
+    cmp ebx, '*'  
     je _mul
-    cmp ebx, '/'
-    je _div
+    cmp ebx, '/' 
+    je _div 
     cmp ebx, "sqrt"
-    je _sqrt
-    ret
+    je _sqrt 
+    ret  ; Return if none of the operations match
+
 _io:
-    push ebp
-    mov ebp, esp
-    push num1
-    call _printf
+    ;function to handle input output
+    push ebp ; save base pointer
+    mov ebp, esp ; set stack frame
 
-    push cint1
-    push formatin
-    call _scanf
+    push num1 ; push num1 data to stack
+    call _printf ; print num1
 
-    push num2
+    push cint1 ; push cint1 address
+    push formatin ; push formatting
+    call _scanf ; accept input
+
+    push num2 ; repeat for number 2
     call _printf
 
     push cint2
     push formatin
     call _scanf
 
-    movsd xmm0, qword [cint1]
+    movsd xmm0, qword [cint1] ; move cint1 & cint2 to xmm registers
     movsd xmm1, qword [cint2]
-    mov esp, ebp
+    mov esp, ebp ; restore stack
     pop ebp
     ret
+
 _return:
-    add esp, 4
-    push retcall
+    add esp, 4 ; adjust stack
+    push retcall ; return text
     call _printf
 
     add esp, 4
-    push retr
-    push wordin
+    push retr ; return adress
+    push wordin ; string formatting
     call _scanf
 
     add esp, 8
     movzx edx, byte [retr]
-    cmp dl, 'Y'
+    cmp dl, 'Y' ; cmp for Y if true, goto main if anything else end
     je _main
     ret
+
 _add:
-    call _io
-    addsd xmm0, xmm1
-    movsd qword [esp], xmm0
-    push numout
+    call _io ; call io function
+    addsd xmm0, xmm1 ; double addition
+    movsd qword [esp], xmm0 ; move to stack
+    push numout ; formatting
     call _printf
-    call _return
+    call _return ; call return function
+
 _sub:
     call _io
     subsd xmm0, xmm1
@@ -102,6 +116,7 @@ _sub:
     push numout
     call _printf
     call _return
+
 _mul:
     call _io
     mulsd xmm0, xmm1
@@ -109,6 +124,7 @@ _mul:
     push numout
     call _printf
     call _return
+
 _div:
     call _io
     divsd xmm0, xmm1
@@ -116,20 +132,22 @@ _div:
     push numout
     call _printf
     call _return
+
 _sqrt:
+    ;sqrt diffrent because it likes to be
     add esp, 8
-    push num0
+    push num0 ; push num0 string 
     call _printf
 
     add esp, 4
-    push cint1
-    push formatin
+    push cint1 ; variable adress
+    push formatin ; formatting
     call _scanf
-    movsd xmm0, qword [cint1]
+    movsd xmm0, qword [cint1] ; mov to xmm0 register
 
     add esp, 8
-    sqrtsd    xmm0, xmm0
-    movsd qword [esp], xmm0
-    push numout
+    sqrtsd    xmm0, xmm0 ; sqrt an store result in xmm0
+    movsd qword [esp], xmm0 ; push to stack
+    push numout ; formatting
     call _printf
     call _return
